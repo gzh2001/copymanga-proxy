fa2ecdd448f1f189d28cc9a8e73dde9abab6e37a
gzhdev <gzhdev@outlook.com>
2024-02-28 21:07:08 +0800

    将APIKEY，HOST放入环境变量

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-from flask import Flask, request, Response
+import os
+import random
+import string
+
 import requests
+from flask import Flask, request, Response
 
 app = Flask(__name__)
+alphabet = string.ascii_letters + string.digits
+
+# 获取环境变量
+app.config["HOST"] = os.getenv("HOST", "hi77-overseas.mangafuna.xyz")
+app.config["APIKEY"] = os.getenv("APIKEY", ''.join(random.sample(string.ascii_letters + string.digits, 32)))
 
 
-# 定义常量
-HOST = "hi77-overseas.mangafuna.xyz"
-APIKEY = "9AsyfnMfmUQNBKGj8sRrbY8T5VgZN3bd8UoL2WXMkbTxNjyXqyu8vjuw6qDck9xs"
+app.logger.info("APIKEY: %s", app.config["APIKEY"])
+
+# HOST = "hi77-overseas.mangafuna.xyz"
+# APIKEY = "9AsyfnMfmUQNBKGj8sRrbY8T5VgZN3bd8UoL2WXMkbTxNjyXqyu8vjuw6qDck9xs"
 
 
 # 此函数用于检查code是否正确
 def check_code(code):
     # 假设有效的code为"12345"
-    return code == APIKEY
+    return code == app.config["APIKEY"]
 
 
 @app.route('/', methods=['GET'])
@@ -30,12 +40,12 @@ def proxy():
     if not check_code(code):
         return Response("Invalid code.", status=403)
 
-    if HOST not in url:
+    if app.config["HOST"] not in url:
         return Response("Invalid url.", status=403)
 
     try:
         # 向目标URL发送GET请求，并获取响应
-        response = requests.get(url, headers={'Host': HOST})
+        response = requests.get(url, headers={'Host': app.config["HOST"]})
 
         # 将目标URL的响应内容返回给客户端
         return Response(response.content, status=response.status_code, content_type=response.headers['Content-Type'])
